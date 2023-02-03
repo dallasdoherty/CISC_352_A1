c544ee616f7c96b85361964da64dfd7b13f4521d
Sophie Ellwood <19se@queensu.ca>
2023-02-03 15:47:43 -0500

    making sophie branch

--- A1/propagators.py
+++ A1/propagators.py
@@ -91,7 +91,6 @@ def prop_FC(csp, newVar=None):
     '''Do forward checking. That is check constraints with
        only one uninstantiated variable. Remember to keep
        track of all pruned variable,value tuple_valss and return '''
-    #IMPLEMENT
     pruned = []
     constraints = []
     
@@ -123,7 +122,6 @@ def prop_GAC(csp, newVar=None):
     '''Do GAC propagation. If newVar is None we do initial GAC enforce
        processing all constraints. Otherwise we do GAC enforce with
        constraints containing newVar on GAC Queue'''
-    #IMPLEMENT
     pruned = []
     GAC_queue = []
     
