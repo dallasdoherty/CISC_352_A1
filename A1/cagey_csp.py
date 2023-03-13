74049e45ca44b7356455be3b747c3a37d421442d
dallasdoherty <76972829+dallasdoherty@users.noreply.github.com>
2023-03-13 04:28:02 -0400

    Update cagey_csp.py

--- A1/cagey_csp.py
+++ A1/cagey_csp.py
@@ -1,12 +1,4 @@
-# =============================
-# Student Names: Sarah, Dallas, and Alex
-# Group ID: 50
-# Date: January 31st, 2023
-# =============================
-# CISC 352 - W23
-# cagey_csp.py
-# desc:
-#
+
 
 #Look for #IMPLEMENT tags in this file.
 '''
